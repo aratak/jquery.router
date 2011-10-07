c6865a22dafa2cbf295988f32dbe0c80a040bf14
Alexey Osipenko <aratak@anahoret.com>
2011-10-07 18:07:42 +0300

    export jquery.router from worked project

--- build/jquery.router.js
+++ build/jquery.router.js
@@ -17,45 +17,97 @@
         }
       };
       Router.prototype.fireCallback = function(location) {
-        return this.callback.apply(this, this.rule.exec(location));
+        return $(__bind(function() {
+          return this.callback.apply(this, this.rule.exec(location));
+        }, this));
       };
       return Router;
     })();
     RouteSet = (function() {
-      RouteSet.prototype.findMatchedRoute = function() {
-        var passRoute;
-        passRoute = null;
-        $.each(this.routes, __bind(function(i) {
-          var route;
-          route = this.routes[i];
-          passRoute = route.match(this.location);
-          return !(passRoute != null);
-        }, this));
-        return passRoute;
+      RouteSet.prototype.findAllMatchedRoutes = function(location, setOfRoutes) {
+        return setOfRoutes.filter(function(route) {
+          return route.match(location) != null;
+        });
       };
-      RouteSet.prototype.process = function() {
-        var passRoute;
-        passRoute = this.findMatchedRoute();
-        if (passRoute != null) {
-          return passRoute.fireCallback(this.location);
+      RouteSet.prototype.findMatchedRoute = function(location, setOfRoutes) {
+        return this.findAllMatchedRoutes(location, setOfRoutes)[0];
+      };
+      RouteSet.prototype.processWithSetOfRoutes = function(location, setOfRoutes, findMethodType) {
+        var findMethod, passRoute, _i, _len, _ref;
+        if (findMethodType == null) {
+          findMethodType = 'one';
         }
+        if (findMethodType === 'one') {
+          findMethod = __bind(function(location, setOfRoutes) {
+            return this.findMatchedRoute(location, setOfRoutes);
+          }, this);
+        } else if ('all') {
+          findMethod = __bind(function(location, setOfRoutes) {
+            return this.findAllMatchedRoutes(location, setOfRoutes);
+          }, this);
+        }
+        _ref = $(findMethod(location, setOfRoutes)).toArray();
+        for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+          passRoute = _ref[_i];
+          console.log(passRoute);
+          passRoute.fireCallback(location);
+        }
+        return true;
+      };
+      RouteSet.prototype.beforeFilterProcess = function(location) {
+        return this.processWithSetOfRoutes(location, this.filters.before_all, 'all');
+      };
+      RouteSet.prototype.requestProcess = function(location) {
+        return this.processWithSetOfRoutes(location, this.routes, 'one');
+      };
+      RouteSet.prototype.afterFilterProcess = function(location) {
+        return this.processWithSetOfRoutes(location, this.filters.after_all, 'all');
+      };
+      RouteSet.prototype.process = function(location) {
+        return this.beforeFilterProcess(location) && this.requestProcess(location) && this.afterFilterProcess(location);
       };
       RouteSet.prototype.addRoute = function(rawMatch, callback) {
         return this.routes.push(new Router(rawMatch, callback));
       };
-      function RouteSet(rawRoutesHash, location) {
-        this.location = location;
+      RouteSet.prototype.addFilter = function(rawMatch, callback, filterPosition) {
+        return this.filters[filterPosition].push(new Router(rawMatch, callback));
+      };
+      RouteSet.prototype.add = function(rawRoutesHash, filter) {
+        if (filter == null) {
+          filter = false;
+        }
+        if (!filter) {
+          return $.each(rawRoutesHash, __bind(function(rawMatch, callback) {
+            return this.addRoute(rawMatch, callback);
+          }, this));
+        } else {
+          return $.each(rawRoutesHash, __bind(function(rawMatch, callback) {
+            return this.addFilter(rawMatch, callback, filter);
+          }, this));
+        }
+      };
+      function RouteSet(rawRoutesHash) {
         this.routes = [];
-        $.each(rawRoutesHash, __bind(function(rawMatch, callback) {
-          return this.addRoute(rawMatch, callback);
-        }, this));
+        this.filters = {
+          after_all: [],
+          before_all: []
+        };
+        this.add(rawRoutesHash);
       }
       return RouteSet;
     })();
     return $.router = function(options) {
       options = $.extend({}, options);
+      $.router.routeset = new RouteSet(options.routes);
+      $.router.filters = function(options, filterType) {
+        if (options == null) {
+          options = {};
+        }
+        return $.router.routeset.add(options, filterType);
+      };
+      $.router.filters(options.after_all, 'after_all');
+      $.router.filters(options.before_all, 'before_all');
       return $.history.init(__bind(function(hash) {
-        $.router.routeset = new RouteSet(options, hash);
         return $.router.routeset.process(hash);
       }, this));
     };
--- src/jquery.router.coffee
+++ src/jquery.router.coffee
@@ -4,35 +4,69 @@
   class Router
     constructor: (@rule, @callback)-> @rule = new RegExp(@rule)
     match: (location)-> @ if @rule.test location
-    fireCallback: (location)-> @callback.apply @, @rule.exec(location)
+    fireCallback: (location)-> $(=> @callback.apply @, @rule.exec(location) )
 
   class RouteSet
-    findMatchedRoute: ->
-      passRoute = null
-      $.each @routes, (i)=>
-        route = @routes[i]
-        passRoute = route.match(@location)
-        !passRoute?
-      passRoute
 
-    process: ->
-      passRoute = @findMatchedRoute()
-      passRoute.fireCallback(@location) if passRoute?
+    findAllMatchedRoutes: (location, setOfRoutes)->
+      setOfRoutes.filter (route)->
+        route.match(location)?
+
+    findMatchedRoute: (location, setOfRoutes)->
+      @findAllMatchedRoutes(location, setOfRoutes)[0]
+
+    processWithSetOfRoutes: (location, setOfRoutes, findMethodType='one')->
+      if findMethodType is 'one'
+        findMethod = (location, setOfRoutes)=> @findMatchedRoute(location, setOfRoutes)
+      else if 'all'
+        findMethod = (location, setOfRoutes)=> @findAllMatchedRoutes(location, setOfRoutes)
+
+      for passRoute in $(findMethod(location, setOfRoutes)).toArray()
+        console.log passRoute
+        passRoute.fireCallback(location)
+      true
+
+    beforeFilterProcess: (location)->
+      @processWithSetOfRoutes(location, @filters.before_all, 'all')
+
+    requestProcess: (location)->
+      @processWithSetOfRoutes(location, @routes, 'one')
+
+    afterFilterProcess: (location)->
+      @processWithSetOfRoutes(location, @filters.after_all, 'all')
+
+    process: (location)->
+      @beforeFilterProcess(location) and @requestProcess(location) and @afterFilterProcess(location)
 
     addRoute: (rawMatch, callback)->
       @routes.push new Router(rawMatch, callback)
 
-    constructor: (rawRoutesHash, @location)->
+    addFilter: (rawMatch, callback, filterPosition)->
+      @filters[filterPosition].push new Router(rawMatch, callback)
+
+    add: (rawRoutesHash, filter=false)->
+      unless filter
+        $.each rawRoutesHash, (rawMatch, callback)=>
+          @addRoute(rawMatch, callback)
+      else
+        $.each rawRoutesHash, (rawMatch, callback)=>
+          @addFilter(rawMatch, callback, filter)
+
+    constructor: (rawRoutesHash)->
       @routes = []
-      $.each rawRoutesHash, (rawMatch, callback)=>
-        @addRoute(rawMatch, callback)
-
-
+      @filters =
+        after_all: []
+        before_all: []
+      @add(rawRoutesHash)
 
   $.router = (options)->
     options = $.extend {}, options
+    $.router.routeset = new RouteSet(options.routes)
+    $.router.filters = (options={}, filterType)-> $.router.routeset.add(options, filterType)
+    $.router.filters(options.after_all, 'after_all')
+    $.router.filters(options.before_all, 'before_all')
+
     $.history.init (hash)=>
-      $.router.routeset = new RouteSet(options, hash)
       $.router.routeset.process(hash)
 
 )(jQuery)
